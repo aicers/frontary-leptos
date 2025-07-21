874690ea73efce251c756336ecc34bdec4b48677
sehkone <sehkone@cluml.com>
2025-07-22 08:40:02 +0900

    Use `update` instead of `set` for signals

--- src/sample.rs
+++ src/sample.rs
@@ -2,8 +2,7 @@ use leptos::{
     IntoView,
     attr::global::{ClassAttribute, OnAttribute},
     component,
-    control_flow::Show,
-    prelude::{ElementChild, Get, GetUntracked, Set, signal},
+    prelude::{ElementChild, Get, Update, signal},
     view,
 };
 
@@ -18,32 +17,19 @@ pub fn Button<'a>(label: &'a str) -> impl IntoView {
     view! {
         // Wrapper with vertical spacing between buttons
         <div class="flex flex-col gap-2">
-            <Show when=move || true>
-                <p>{ move || format!("Count Left: {}", count_left.get()) }</p>
-            </Show>
-            <Show when=move || true>
-                <p>{ move || format!("Count Right: {}", count_right.get()) }</p>
-            </Show>
-
-            // <p>{ move || {
-            //     let cnt = count_left.get();
-            //     format!("Count Left: {cnt}")
-            // }}</p>
-            // <p>{ move || {
-            //     let cnt = count_right.get();
-            //     format!("Count Right: {cnt}")
-            // }}</p>
+            <p> "Count Left: " { move || count_left.get() } </p>
+            <p> "Count Right: " { move || count_right.get() } </p>
 
             // Method 1: Use Tailwind utility classes directly
             <button class="bg-primary text-white font-sans px-4 py-2 rounded-xl"
-                on:click=move |_| {set_count_left.set(count_left.get_untracked() + 1);}
+                on:click=move |_| { set_count_left.update(|v| *v += 1); }
             >
                 {label} " (utility)"
             </button>
 
             // Method 2: Use a custom class defined in input.frontary.css
             <button class="btn-primary"
-                on:click=move |_| {set_count_right.set(count_right.get_untracked() + 1);}
+                on:click=move |_| { set_count_right.update(|v| *v += 1); }
             >
                 {label} " (custom)"
             </button>
