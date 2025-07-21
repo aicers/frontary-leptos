7ccf114787c7df1cb7c3c1fa7b71ec3981c898e7
Sehkone Kim <55010932+sehkone@users.noreply.github.com>
2025-07-22 08:48:25 +0900

    Avoid `get_untracked` by using `update`
    While another signal handling to `sample::button`
    was added, this commit replaced `set` with `update`.

--- src/sample.rs
+++ src/sample.rs
@@ -2,7 +2,7 @@ use leptos::{
     IntoView,
     attr::global::{ClassAttribute, OnAttribute},
     component,
-    prelude::{ElementChild, Get, GetUntracked, Set, signal},
+    prelude::{ElementChild, Get, Update, signal},
     view,
 };
 
@@ -11,23 +11,26 @@ use leptos::{
 #[allow(clippy::must_use_candidate)]
 #[component]
 pub fn Button<'a>(label: &'a str) -> impl IntoView {
-    let (count, set_count) = signal(0);
+    let (count_left, set_count_left) = signal(0);
+    let (count_right, set_count_right) = signal(0);
 
     view! {
         // Wrapper with vertical spacing between buttons
         <div class="flex flex-col gap-2">
-
-            <p> "Count: " { move || count.get() } </p>
+            <p> "Count Left: " { move || count_left.get() } </p>
+            <p> "Count Right: " { move || count_right.get() } </p>
 
             // Method 1: Use Tailwind utility classes directly
             <button class="bg-primary text-white font-sans px-4 py-2 rounded-xl"
-                on:click=move |_| {set_count.set(count.get_untracked() + 1);}
+                on:click=move |_| { set_count_left.update(|v| *v += 1); }
             >
                 {label} " (utility)"
             </button>
 
             // Method 2: Use a custom class defined in input.frontary.css
-            <button class="btn-primary">
+            <button class="btn-primary"
+                on:click=move |_| { set_count_right.update(|v| *v += 1); }
+            >
                 {label} " (custom)"
             </button>
 
