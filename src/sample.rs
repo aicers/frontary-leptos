b6d715ec16303f5dad72c8089a552c4105427cc9
sehkone <sehkone@cluml.com>
2025-07-22 08:05:18 +0900

    Add another signal handling to `sample::button`

--- src/sample.rs
+++ src/sample.rs
@@ -2,6 +2,7 @@ use leptos::{
     IntoView,
     attr::global::{ClassAttribute, OnAttribute},
     component,
+    control_flow::Show,
     prelude::{ElementChild, Get, GetUntracked, Set, signal},
     view,
 };
@@ -11,23 +12,39 @@ use leptos::{
 #[allow(clippy::must_use_candidate)]
 #[component]
 pub fn Button<'a>(label: &'a str) -> impl IntoView {
-    let (count, set_count) = signal(0);
+    let (count_left, set_count_left) = signal(0);
+    let (count_right, set_count_right) = signal(0);
 
     view! {
         // Wrapper with vertical spacing between buttons
         <div class="flex flex-col gap-2">
+            <Show when=move || true>
+                <p>{ move || format!("Count Left: {}", count_left.get()) }</p>
+            </Show>
+            <Show when=move || true>
+                <p>{ move || format!("Count Right: {}", count_right.get()) }</p>
+            </Show>
 
-            <p> "Count: " { move || count.get() } </p>
+            // <p>{ move || {
+            //     let cnt = count_left.get();
+            //     format!("Count Left: {cnt}")
+            // }}</p>
+            // <p>{ move || {
+            //     let cnt = count_right.get();
+            //     format!("Count Right: {cnt}")
+            // }}</p>
 
             // Method 1: Use Tailwind utility classes directly
             <button class="bg-primary text-white font-sans px-4 py-2 rounded-xl"
-                on:click=move |_| {set_count.set(count.get_untracked() + 1);}
+                on:click=move |_| {set_count_left.set(count_left.get_untracked() + 1);}
             >
                 {label} " (utility)"
             </button>
 
             // Method 2: Use a custom class defined in input.frontary.css
-            <button class="btn-primary">
+            <button class="btn-primary"
+                on:click=move |_| {set_count_right.set(count_right.get_untracked() + 1);}
+            >
                 {label} " (custom)"
             </button>
 
